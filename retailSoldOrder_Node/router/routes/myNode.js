f46d4b2fa83f26d287b874d1badc58ab6cb4ff94
Anthony Wat <anthony_wat@toyota.ca>
2019-01-31 15:29:22 -0500

    Added support for proxied calls to IBM APIC via APIM
    - Requires adding the apicClientId and apicClientSecret properties to
    the APIM user-defined service definition

--- retailSoldOrder_Node/router/routes/myNode.js
+++ retailSoldOrder_Node/router/routes/myNode.js
@@ -1,13 +1,9 @@
 /*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0*/
 /*eslint-env node, es6 */
-'use strict';
-var express = require('express');
-var request = require('request');
+"use strict";
+var express = require("express");
+var request = require("request");
 var xsenv = require("@sap/xsenv");
-var passport = require('passport');
-var JWTStrategy = require('@sap/xssec').JWTStrategy;
-
-var async = require('async');
 
 var app = express();
 
@@ -21,7 +17,7 @@ module.exports = function () {
 	var options = {};
 	options = Object.assign(options, xsenv.getServices({
 		api: {
-				name: "RETAIL_SOLD_ORDER_APIM_CUPS" 
+			name: "RETAIL_SOLD_ORDER_APIM_CUPS"
 		}
 	}));
 
@@ -29,11 +25,13 @@ module.exports = function () {
 		pwd = options.api.password,
 		url = options.api.host,
 		APIKey = options.api.APIKey,
-		client = options.api.client;
+		client = options.api.client,
+		apicClientId = options.api.apicClientId,
+		apicClientSecret = options.api.apicClientSecret;
 
-	console.log('The API Management URL', url);
+	console.log("The API Management URL", url);
 
-	var auth64 = 'Basic ' + new Buffer(uname + ':' + pwd).toString('base64');
+	var auth64 = "Basic " + new Buffer(uname + ":" + pwd).toString("base64");
 
 	var reqHeader = {
 		"Authorization": auth64,
@@ -52,86 +50,110 @@ module.exports = function () {
 
 	var csrfToken;
 
-	app.all('/*', function (req, res, next) {
-
-		let headOptions = {};
-
-		headOptions.Authorization = auth64;
-
-		let method = req.method;
-		let xurl = url + req.url;
-		console.log('Method', method);
-		console.log('Incoming Url', xurl);
-		console.log('csrfToken before GET&POST', csrfToken);
-
-		// console.log(req.headers.cookie);
-		//  delete (req.headers.cookie);
-		//   console.log(req.headers.cookie);
-
-		if (method == 'GET') {
-		
-
-			var reqHeader = {
-				"Authorization": auth64,
-				"Content-Type": "application/json",
+	app.all("/*", function (req, res, next) {
+		// Proxied call is to IBM APIC
+		if (req.url.startsWith("/tci/internal")) {
+			var proxiedReqHeaders = {
 				"APIKey": APIKey,
-				"x-csrf-token": "Fetch"
+				"Content-Type": req.get("Content-Type"),
+				"x-ibm-client-id": apicClientId,
+				"x-ibm-client-secret": apicClientSecret
 			};
-
-		}
-
-		//  if the method = post you need a csrf token.   
-
-		if (method == 'POST' || method == 'DELETE' || method == 'PUT' || method == 'HEAD') {
-			reqHeader = {
-				"Authorization": auth64,
-				"Content-Type": "application/json",
-				"APIKey": APIKey,
-				"x-csrf-token": csrfToken
-			};
-			console.log('csrfToken for POST', csrfToken);
-			console.log('headerData', reqHeader);
-		}
-
-		let xRequest =
-			request({
-				method: method,
-				url: xurl,
-				headers: reqHeader
+			var proxiedMethod = req.method;
+			var proxiedUrl = url + req.url;
+			let proxiedReq = request({
+				headers: proxiedReqHeaders,
+				method: proxiedMethod,
+				url: proxiedUrl
 			});
+			req.pipe(proxiedReq);
+			proxiedReq.on("response", proxiedRes => {
+				delete(proxiedRes.headers.cookie);
+				proxiedReq.pipe(res);
+			}).on("error", error => {
+				next(error);
+			});
+		}
 
-		req.pipe(xRequest);
+		// Proxied call is to S4/HANA
+		else {
+			let headOptions = {};
 
-		xRequest.on('response', (response) => {
+			headOptions.Authorization = auth64;
 
-			delete(response.headers.cookie);
+			let method = req.method;
+			let xurl = url + req.url;
+			console.log("Method", method);
+			console.log("Incoming Url", xurl);
+			console.log("csrfToken before GET&POST", csrfToken);
 
-			if (response.headers['x-csrf-token']) {
-				if (response.headers['x-csrf-token'] !== 'Required') {
-					csrfToken = response.headers['x-csrf-token'];
-					console.log("csrfToken received from SAP");
-				} else {
-					console.log("Csrf is received as Required.");
+			// console.log(req.headers.cookie);
+			//  delete (req.headers.cookie);
+			//   console.log(req.headers.cookie);
+
+			if (method == "GET") {
+
+				var reqHeader = {
+					"Authorization": auth64,
+					"Content-Type": "application/json",
+					"APIKey": APIKey,
+					"x-csrf-token": "Fetch"
+				};
+
+			}
+
+			//  if the method = post you need a csrf token.   
+
+			if (method == "POST" || method == "DELETE" || method == "PUT" || method == "HEAD") {
+				reqHeader = {
+					"Authorization": auth64,
+					"Content-Type": "application/json",
+					"APIKey": APIKey,
+					"x-csrf-token": csrfToken
+				};
+				console.log("csrfToken for POST", csrfToken);
+				console.log("headerData", reqHeader);
+			}
+
+			let xRequest =
+				request({
+					method: method,
+					url: xurl,
+					headers: reqHeader
+				});
+
+			req.pipe(xRequest);
+
+			xRequest.on("response", (response) => {
+
+				delete(response.headers.cookie);
+
+				if (response.headers["x-csrf-token"]) {
+					if (response.headers["x-csrf-token"] !== "Required") {
+						csrfToken = response.headers["x-csrf-token"];
+						console.log("csrfToken received from SAP");
+					} else {
+						console.log("Csrf is received as Required.");
+					}
+
+				}
+				console.log("csrfToken NOT received for", method);
+
+				if (method == "GET" && !(response.headers["x-csrf-token"])) {
+					csrfToken = csrfToken; //self assign this to retain the value. 
+					console.log("The earlier call returned blank CSRF and so we are reusing this one", csrfToken);
 				}
 
-			}
-			console.log("csrfToken NOT received for", method);
-			
-			if (method == 'GET' && !(response.headers['x-csrf-token']) ) {
-				csrfToken = csrfToken;  //self assign this to retain the value. 
-				console.log ("The earlier call returned blank CSRF and so we are reusing this one", csrfToken);
-			}
+				console.log("Response from sap Received Success and if csrf available it will be here & Csrf Token", method, csrfToken);
 
-			console.log('Response from sap Received Success and if csrf available it will be here & Csrf Token', method, csrfToken);
+				xRequest.pipe(res);
 
-			xRequest.pipe(res);
+			}).on("error", (error) => {
+				next(error);
 
-		}).on('error', (error) => {
-			next(error);
-			
-			console.log("This is inside error");
-		});
-
+				console.log("This is inside error");
+			});
+		}
 	});
 
 	return app;
