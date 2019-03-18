3ec89b1f61bf6e1b865994f78c291379d99eab37
Mohamed Medhat <mohamed_medhat@toyota.ca>
2019-03-18 18:10:33 +0000

    new version

--- soldOrder/webapp/controller/BaseController.js
+++ soldOrder/webapp/controller/BaseController.js
@@ -1,9 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
+	
 	/*"sap/ui/model/odata/oDataModel",*/
 	"/sap/ui/model/resource/ResourceModel",
-	"sap/ui/core/routing/History"
+	"sap/ui/core/routing/History",
+	"sap/ui/model/json/JSONModel"
 ], function (Controller, ResourceModel, History,JSONModel) {
 	"use strict";
 	var basCont;
