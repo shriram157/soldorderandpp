5a268391253277ca72feb0741634d930d7d5e138
Saraogi Vivek <vivek_saraogi@toyota.ca>
2019-05-16 18:18:03 +0000

    Changes for change order

--- soldOrder/webapp/controller/ChangeSoldOrderRequest.controller.js
+++ soldOrder/webapp/controller/ChangeSoldOrderRequest.controller.js
@@ -101,8 +101,8 @@ sap.ui.define([
 			var valSuffix = CSOR_controller.getView().byId("suffix_CSOR").getSelectedKey();
 			var valApx = CSOR_controller.getView().byId("apx_CSOR").getSelectedKey();
 			var valColour = CSOR_controller.getView().byId("colour_CSOR").getSelectedKey();
-			var valFrom = CSOR_controller.getView().byId("etaFrom_CSOR").getValue();
-			var valTo = CSOR_controller.getView().byId("etaTo_CSOR").getValue();
+			var valFrom = CSOR_controller.getView().byId("etaFrom_CSOR").getText();
+			var valTo = CSOR_controller.getView().byId("etaTo_CSOR").getText();
 			var Zzmoyr = CSOR_controller.getView().byId("moyr_CSOR").getText();
 			var Zzseries = CSOR_controller.getView().byId("series_CSOR").getSelectedKey();
 			var ZcontractDate1 = CSOR_controller.getView().byId("conDate_CSOR").getText();
