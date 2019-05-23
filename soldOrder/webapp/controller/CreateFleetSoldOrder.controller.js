212eb053e38e33ac00fda3affa9d9cc26ecdafb0
Saraogi Vivek <vivek_saraogi@toyota.ca>
2019-05-23 11:36:03 +0000

    changes for numeric value validation for quantity

--- soldOrder/webapp/controller/CreateFleetSoldOrder.controller.js
+++ soldOrder/webapp/controller/CreateFleetSoldOrder.controller.js
@@ -776,10 +776,13 @@ sap.ui.define([
 			isNumberKey: function (evt)
       {
       	 evt = (evt) ? evt : window.event;
-         var charCode = (evt.which) ? evt.which : evt.keyCode
+         var charCode = (evt.which) ? evt.which : evt.keyCode;
          if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+      {
+      		sap.m.MessageBox.show("Please enter a numeric value", sap.m.MessageBox.Icon.ERROR, "Error", sap
+						.m.MessageBox.Action.OK, null, null);
             return false;
-
+		}
          return true;
       },
 		onCloseDialogFan: function (Oevent) {
