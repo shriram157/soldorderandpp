634e633b87baf7f9306302369052bd918131ada4
Minakshi Singh <minakshi_singh@toyota.ca>
2021-03-05 00:54:13 +0000

    changes done for fllet_national role

--- retailSoldOrder_Node/router/routes/user-details.js
+++ retailSoldOrder_Node/router/routes/user-details.js
@@ -276,7 +276,7 @@ module.exports = function (appContext) {
 		} else if (approveFleetSoldOrder && approvePriceProtection && !manageFleetSoldOrder && !manageRetailSoldOrder && viewFleetSoldOrder &&
 			viewPriceProtection &&
 			viewRetailSoldOrder) {
-			role = userAttributes.National ? "National_Fleet_User" : "TCI_User";
+			role = userAttributes.UserType ? "TCI_User" : "National_Fleet_User";
 		} else if (approveFleetSoldOrder && !approvePriceProtection && !manageFleetSoldOrder && !manageRetailSoldOrder && viewFleetSoldOrder &&
 			viewPriceProtection &&
 			viewRetailSoldOrder) {
