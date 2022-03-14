417a67ef807b3800cdfa2343665a2bf9c2407446
Minakshi Singh <minakshi_singh@toyota.ca>
2022-03-14 17:33:13 +0000

    changes done for export

--- soldOrder/webapp/controller/PriceProtectionDetails_Dealer.controller.js
+++ soldOrder/webapp/controller/PriceProtectionDetails_Dealer.controller.js
@@ -4,10 +4,15 @@ sap.ui.define([
 	"sap/ui/model/Sorter",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
-], function (BaseController, formatter, Sorter, Filter, FilterOperator) {
+	"sap/ui/core/util/Export",
+	"sap/ui/core/util/ExportTypeCSV",
+	'sap/ui/export/library',
+	'sap/ui/export/Spreadsheet'
+], function (BaseController, formatter, Sorter, Filter, FilterOperator, Export, ExportTypeCSV, exportLibrary, Spreadsheet) {
 	"use strict";
 	var PPD_DealerCont, clicks = 0,
 		num = 0,
+		oUrl,
 		pricepro = false;
 	var language = sap.ui.getCore().getModel("i18n").getResourceBundle().sLocale.toLocaleUpperCase();
 	return BaseController.extend("toyota.ca.SoldOrder.controller.PriceProtectionDetails_Dealer", {
@@ -24,28 +29,28 @@ sap.ui.define([
 					"PriceProtectionStatus": [{
 						"key": "APPROVED",
 						"text": "APPROVED"
-					},{
+					}, {
 						"key": "CANCELLED",
 						"text": "CANCELLED"
-					},{
+					}, {
 						"key": "CHANGED",
 						"text": "CHANGED"
-					},{
+					}, {
 						"key": "CLOSED",
 						"text": "CLOSED"
-					},{
+					}, {
 						"key": "IN PROGRESS",
 						"text": "IN PROGRESS"
-					},{
+					}, {
 						"key": "OPEN",
 						"text": "OPEN"
-					},{
+					}, {
 						"key": "PRE-APPROVED",
 						"text": "PRE-APPROVED"
-					},{
+					}, {
 						"key": "REJECTED",
 						"text": "REJECTED"
-					},{
+					}, {
 						"key": "UNDER-REVIEW",
 						"text": "UNDER-REVIEW"
 					}]
@@ -55,28 +60,28 @@ sap.ui.define([
 					"PriceProtectionStatus": [{
 						"key": "APPROVED",
 						"text": "APPROVED"
-					},{
+					}, {
 						"key": "CANCELLED",
 						"text": "CANCELLED"
-					},{
+					}, {
 						"key": "CHANGED",
 						"text": "CHANGED"
-					},{
+					}, {
 						"key": "CLOSED",
 						"text": "CLOSED"
-					},{
+					}, {
 						"key": "IN PROGRESS",
 						"text": "IN PROGRESS"
-					},{
+					}, {
 						"key": "OPEN",
 						"text": "OPEN"
-					},{
+					}, {
 						"key": "PRE-APPROVED",
 						"text": "PRE-APPROVED"
-					},{
+					}, {
 						"key": "REJECTED",
 						"text": "REJECTED"
-					},{
+					}, {
 						"key": "UNDER-REVIEW",
 						"text": "UNDER-REVIEW"
 					}]
@@ -179,9 +184,9 @@ sap.ui.define([
 			var mcb_ordTyp_PPD_D = PPD_DealerCont.getView().byId("mcb_ordTyp_PPD_D");
 			var mcb_dealer_PPD_D = PPD_DealerCont.getView().byId("mcb_dealer_PPD_D");
 			// changes done for demand DMND0003456 by Minakshi
-			var aSelectedStatusArr = mcb_status_PPD_D.getItems().filter(item => 
+			var aSelectedStatusArr = mcb_status_PPD_D.getItems().filter(item =>
 				item.getKey() == "OPEN" || item.getKey() == "PRE-APPROVED" ||
-				item.getKey() == "CHANGED" || item.getKey() == "UNDER-REVIEW" || item.getKey() == "IN PROGRESS" 
+				item.getKey() == "CHANGED" || item.getKey() == "UNDER-REVIEW" || item.getKey() == "IN PROGRESS"
 			);
 			mcb_status_PPD_D.setSelectedItems(aSelectedStatusArr);
 			mcb_dealer_PPD_D.setSelectedItems(mcb_dealer_PPD_D.getItems());
@@ -207,16 +212,14 @@ sap.ui.define([
 				PPD_DealerCont.getView().byId("cb_dealer_PPD_D").setSelectedKey("");
 			}
 			//Changes done for INC0189944 by Minakshi end.
-			
-		},
 
-	
+		},
 
 		_refresh: function () {
 
 			var x = sap.ui.getCore().getModel("LoginUserModel").getProperty("/UserType");
 			if (x != "TCI_User" && x !== "TCI_Zone_User" && x !== "National_Fleet_User") {
-				var oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=0&$filter=(";
+				 oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=0&$filter=(";
 				for (var i = 0; i < this.getView().byId("mcb_status_PPD_D").getSelectedItems().length; i++) {
 					var status = this.getView().byId("mcb_status_PPD_D").getSelectedItems()[i].getKey();
 					oUrl = oUrl + "(status eq '" + status + "')";
@@ -245,8 +248,8 @@ sap.ui.define([
 						oUrl = oUrl + " or ";
 					}
 				}
-					//	DMND0003455 changes done by Minakshi 13/12/2021
-				oUrl = oUrl+"and expiry ne 'X'" + "&$orderby=dealer_ord desc";
+				//	DMND0003455 changes done by Minakshi 13/12/2021
+				oUrl = oUrl + "and expiry ne 'X'" + "&$orderby=dealer_ord desc";
 				$.ajax({
 					url: oUrl,
 					method: "GET",
@@ -283,7 +286,7 @@ sap.ui.define([
 				});
 			} else {
 				if (pricepro == true) {
-					var oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=0&$filter=(";
+					 oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=0&$filter=(";
 					for (var i = 0; i < this.getView().byId("mcb_status_PPD_D").getSelectedItems().length; i++) {
 						var status = this.getView().byId("mcb_status_PPD_D").getSelectedItems()[i].getKey();
 						oUrl = oUrl + "(status eq '" + status + "')";
@@ -305,8 +308,8 @@ sap.ui.define([
 					}
 					var dealer = this.getView().byId("cb_dealer_PPD_D").getSelectedKey();
 					oUrl = oUrl + "(dealer_code eq '" + dealer + "'))";
-	//	DMND0003455 changes done by Minakshi 13/12/2021
-					oUrl = oUrl+"and expiry ne 'X'" + "&$orderby=dealer_ord desc";
+					//	DMND0003455 changes done by Minakshi 13/12/2021
+					oUrl = oUrl + "and expiry ne 'X'" + "&$orderby=dealer_ord desc";
 
 					$.ajax({
 						url: oUrl,
@@ -345,7 +348,7 @@ sap.ui.define([
 					});
 				} else {
 
-					var oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=0&$filter=(";
+					 oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=0&$filter=(";
 					for (var i = 0; i < this.getView().byId("mcb_status_PPD_D").getSelectedItems().length; i++) {
 						var status = this.getView().byId("mcb_status_PPD_D").getSelectedItems()[i].getKey();
 						oUrl = oUrl + "(status eq '" + status + "')";
@@ -365,8 +368,8 @@ sap.ui.define([
 							oUrl = oUrl + " or ";
 						}
 					}
-						//	DMND0003455 changes done by Minakshi 13/12/2021
-					oUrl = oUrl+"and expiry ne 'X'" + "&$orderby=dealer_ord desc";
+					//	DMND0003455 changes done by Minakshi 13/12/2021
+					oUrl = oUrl + "and expiry ne 'X'" + "&$orderby=dealer_ord desc";
 					$.ajax({
 						url: oUrl,
 						method: "GET",
@@ -408,7 +411,7 @@ sap.ui.define([
 		_refreshCombo: function (evt) {
 			pricepro = true;
 			PPD_DealerCont.dialog.open();
-			var oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=0&$filter=(";
+			 oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=0&$filter=(";
 			for (var i = 0; i < this.getView().byId("mcb_status_PPD_D").getSelectedItems().length; i++) {
 				var status = this.getView().byId("mcb_status_PPD_D").getSelectedItems()[i].getKey();
 				oUrl = oUrl + "(status eq '" + status + "')";
@@ -430,8 +433,8 @@ sap.ui.define([
 			}
 			var dealer = this.getView().byId("cb_dealer_PPD_D").getSelectedKey();
 			oUrl = oUrl + "(dealer_code eq '" + dealer + "'))";
-	//	DMND0003455 changes done by Minakshi 13/12/2021
-			oUrl = oUrl+"and expiry ne 'X'" + "&$orderby=dealer_ord desc";
+			//	DMND0003455 changes done by Minakshi 13/12/2021
+			oUrl = oUrl + "and expiry ne 'X'" + "&$orderby=dealer_ord desc";
 
 			$.ajax({
 				url: oUrl,
@@ -488,7 +491,7 @@ sap.ui.define([
 
 		},
 		_handleServiceSuffix_Series: function (nodeJsUrl) {
-			var oUrl = nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/SoldOrderSeriesSet?$format=json";
+			 oUrl = nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/SoldOrderSeriesSet?$format=json";
 			$.ajax({
 				url: oUrl,
 				method: 'GET',
@@ -523,7 +526,7 @@ sap.ui.define([
 
 			var x = sap.ui.getCore().getModel("LoginUserModel").getProperty("/UserType");
 			if (x != "TCI_User" && x !== "TCI_Zone_User" && x !== "National_Fleet_User") {
-				var oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=" + num + "&$filter=(";
+				 oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=" + num + "&$filter=(";
 				for (var i = 0; i < this.getView().byId("mcb_status_PPD_D").getSelectedItems().length; i++) {
 					var status = this.getView().byId("mcb_status_PPD_D").getSelectedItems()[i].getKey();
 					oUrl = oUrl + "(status eq '" + status + "')";
@@ -552,8 +555,8 @@ sap.ui.define([
 						oUrl = oUrl + " or ";
 					}
 				}
-			//	DMND0003455 changes done by Minakshi 13/12/2021
-				oUrl = oUrl+"and expiry ne 'X'" + "&$orderby=dealer_ord desc";
+				//	DMND0003455 changes done by Minakshi 13/12/2021
+				oUrl = oUrl + "and expiry ne 'X'" + "&$orderby=dealer_ord desc";
 				$.ajax({
 					url: oUrl,
 					method: "GET",
@@ -590,7 +593,7 @@ sap.ui.define([
 				});
 			} else {
 				if (pricepro == false) {
-					var oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=" + num + "&$filter=(";
+					 oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=" + num + "&$filter=(";
 					for (var i = 0; i < this.getView().byId("mcb_status_PPD_D").getSelectedItems().length; i++) {
 						var status = this.getView().byId("mcb_status_PPD_D").getSelectedItems()[i].getKey();
 						oUrl = oUrl + "(status eq '" + status + "')";
@@ -610,8 +613,8 @@ sap.ui.define([
 							oUrl = oUrl + " or ";
 						}
 					}
-						//	DMND0003455 changes done by Minakshi 13/12/2021
-					oUrl = oUrl+"and expiry ne 'X'" + "&$orderby=dealer_ord desc";
+					//	DMND0003455 changes done by Minakshi 13/12/2021
+					oUrl = oUrl + "and expiry ne 'X'" + "&$orderby=dealer_ord desc";
 					$.ajax({
 						url: oUrl,
 						method: "GET",
@@ -648,7 +651,7 @@ sap.ui.define([
 					});
 				} else {
 
-					var oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=" + num + "&$filter=(";
+					 oUrl = this.nodeJsUrl + "/ZVMS_SOLD_ORDER_SRV/ZVMS_CDS_PRC_PRTC_Eligible?$top=100&$skip=" + num + "&$filter=(";
 					for (var i = 0; i < this.getView().byId("mcb_status_PPD_D").getSelectedItems().length; i++) {
 						var status = this.getView().byId("mcb_status_PPD_D").getSelectedItems()[i].getKey();
 						oUrl = oUrl + "(status eq '" + status + "')";
@@ -670,7 +673,7 @@ sap.ui.define([
 					}
 					var dealer = this.getView().byId("cb_dealer_PPD_D").getSelectedKey();
 					oUrl = oUrl + "(dealer_code eq '" + dealer + "'))";
-					oUrl = oUrl+"and expiry ne 'X'" + "&$orderby=dealer_ord desc"; //Changed by singhmi 05/02/2021
+					oUrl = oUrl + "and expiry ne 'X'" + "&$orderby=dealer_ord desc"; //Changed by singhmi 05/02/2021
 
 					$.ajax({
 						url: oUrl,
@@ -738,6 +741,111 @@ sap.ui.define([
 				aFilters = new sap.ui.model.Filter([oFilter], true);
 			}
 			this.byId("table_PPD_Dealer").getBinding().filter(aFilters).sort(aSorters);
+		},
+
+		onExport: function (oEvent) {
+			var aCols, oRowBinding, oSettings, oSheet, oTable, icount, sUri, sfilter = "",
+				sorderby, sSelect, iskip;
+			// if (!this._oTable) {
+			// 	this._oTable = this.byId("list");
+			// }
+
+			// oTable = this._oTable;
+			//oRowBinding = oTable.getBinding("items");
+			aCols = this.createColumnConfig();
+			// icount = oRowBinding.aContexts.length;
+			// iskip = (icount > 100) ? icount - 100 : 0;
+			oSettings = {
+				workbook: {
+					columns: aCols
+				},
+				dataSource: {
+					dataUrl: oUrl
+					
+				},
+				fileName: 'pricingData.xlsx',
+				worker: false
+
+			};
+			
+			//count: icount
+
+			oSheet = new Spreadsheet(oSettings);
+			oSheet.build().finally(function () {
+				oSheet.destroy();
+			});
+		},
+		createColumnConfig: function () {
+			
+			var EdmType = exportLibrary.EdmType;
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var aCols = [];
+			aCols.push({
+				label: oBundle.getText("orderNum"),
+				type: EdmType.String,
+				property: 'dealer_ord'
+			});
+
+			aCols.push({
+				label: oBundle.getText("custname"),
+				type: EdmType.String,
+				property: 'zzendcu_name'
+			});
+
+			aCols.push({
+				label: oBundle.getText("rdrCustName"),
+				type: EdmType.String,
+				property: 'zzrdrcust_name'
+			});
+			aCols.push({
+				label: oBundle.getText("orderType"),
+				type: EdmType.String,
+				property: 'zzordtypedesc'
+			});
+			aCols.push({
+				label: oBundle.getText("modelYear"),
+				type: EdmType.String,
+				property: 'zzmoyr'
+			});
+
+			aCols.push({
+				label: oBundle.getText("series"),
+				type: EdmType.String,
+				property: 'zzseries'
+			});
+
+			aCols.push({
+				label: oBundle.getText("vin"),
+				type: EdmType.String,
+				property: 'vhvin'
+			});
+
+			aCols.push({
+				label: oBundle.getText("vtn"),
+				type: EdmType.String,
+				property: 'zzvtn'
+			});
+
+			aCols.push({
+				label: oBundle.getText("status"),
+				type: EdmType.String,
+				property: 'status'
+			});
+
+			aCols.push({
+				label: oBundle.getText("OwnershipUploaded"),
+				type: EdmType.String,
+				property: 'ownership_doc'
+			});
+
+			aCols.push({
+				label: oBundle.getText("CreditMemo"),
+				type: EdmType.String,
+				property: 'credit_memo_doc'
+			});
+
+			return aCols;
+
 		}
 	});
 });
