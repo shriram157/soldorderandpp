569cd45dc287d6b791cee3b3612350b714fee46f
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2022-12-08 10:34:01 +0000

    changes for TASK0179454

--- soldOrder/webapp/controller/RSOView_ManageSoldOrder.controller.js
+++ soldOrder/webapp/controller/RSOView_ManageSoldOrder.controller.js
@@ -484,13 +484,13 @@ sap.ui.define([
 					RSO_MSO_controller.getView().byId("btn_cancelOrder_RSO_MSO").setEnabled(false); //changes by swetha
 				}
 				//changes by swetha for service task TASK0179454 on 9/11/2022.
-				var PStatus = RSO_MSO_controller.getView().getElementBinding('mainservices').getBoundContext().getProperty('PriceStatus');
-				if (PStatus == "APPROVED" || PStatus == "PRE-APPROVED" || PStatus == "UNDER-REVIEW" ) {
+				//var PStatus = RSO_MSO_controller.getView().getElementBinding('mainservices').getBoundContext().getProperty('PriceStatus');
+				if (status == "APPROVED" || status == "PRE-APPROVED" || status == "UNDER-REVIEW" ) {
 					RSO_MSO_controller.getView().byId("btn_ApprPriceProt_RSO_MSO").setEnabled(true);
 				} else {
 					RSO_MSO_controller.getView().byId("btn_ApprPriceProt_RSO_MSO").setEnabled(false);	
 				}
-				if (PStatus == "CLOSED" || PStatus == "DEMO-VEHICLE" || PStatus == "REJECTED" || PStatus == "") {
+				if (status == "CLOSED" || status == "DEMO-VEHICLE" || status == "REJECTED" || status == "") {
 					RSO_MSO_controller.getView().byId("btn_RejPriceProt_RSO_MSO").setEnabled(false);	
 				} else {
 					RSO_MSO_controller.getView().byId("btn_RejPriceProt_RSO_MSO").setEnabled(true);		
