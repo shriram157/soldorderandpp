a56ed9a592857bd4451752c805e8d3c2f9763715
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2023-09-20 08:58:22 +0000

    changes for fragment for DMND0003239

--- soldOrder/webapp/controller/RSOView_ManageSoldOrder.controller.js
+++ soldOrder/webapp/controller/RSOView_ManageSoldOrder.controller.js
@@ -1365,8 +1365,9 @@ sap.ui.define([
 			this.getView().addDependent(oDialogBox);
 			oDialogBox.open();
 		},
-		onClose: function(oEvent) {
-    		oEvent.getSource().getParent().getParent().close();
+		onClose: function() {
+    		var dialog = RSO_MSO_controller.getView().byId("RDRVIN_dialog");
+			dialog.close();
 		}
 		//changes by swetha for DMND0003239 added fragment on click of Link RDR VIN button on 19th Sept, 2023-----End	
 		});
