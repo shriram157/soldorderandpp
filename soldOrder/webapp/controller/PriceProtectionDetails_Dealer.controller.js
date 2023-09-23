4d34f7453771d0b02356cdee7383c02ca93a5a5e
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2023-09-23 13:02:08 +0000

    changes reverted back

--- soldOrder/webapp/controller/PriceProtectionDetails_Dealer.controller.js
+++ soldOrder/webapp/controller/PriceProtectionDetails_Dealer.controller.js
@@ -149,29 +149,7 @@ sap.ui.define([
 			OrderTypeModel.updateBindings(true);
 			sap.ui.getCore().setModel(OrderTypeModel, "OrderTypeModel");
 			this.getView().setModel(OrderTypeModel, "OrderTypeModel");
-			//changes by swetha for DMND0003239 ---- added series filter on 23-09-23----Start
-			var PPDseriesModel = new sap.ui.model.json.JSONModel();
-			var data = sap.ui.getCore().getModel("PPDseriesModel").getData();
-			PPDseriesModel.setData(data);
-			RSOS_controller.getView().setModel(PPDseriesModel, "PPDseriesModel");                   
-			if (data[0].ModelSeriesNo !== "ALL") {                            
-				PPDseriesModel.getData().unshift({
-					"Division": "",
-					"ModelSeriesNo": "ALL",
-					"ProductHierarchy": "",
-					"ProfitCenter": "",
-					"SeriesSequenceNumber": "",
-					"TCIModelDescriptionEN": "",
-					"TCIModelDescriptionFR": "",
-					"TCISeriesDescriptionEN": "All",
-					"TCISeriesDescriptionFR": "Toute",
-					"zzzadddata2": "X",
-					"zzzadddata4": "100"
-				});
-			}
-			PPD_DealerCont.getView().getModel("PPDseriesModel").updateBindings(true);
-			PPD_DealerCont.getOwnerComponent().getModel("LocalDataModel").setProperty("/Lang", language);
-			//changes by swetha for DMND0003239 ---- added series filter on 23-09-23----End
+			
 			// PPD_DealerCont.getBrowserLanguage();
 			this._fnInitDataLoad();
 			this.getOwnerComponent().getRouter().getRoute("PriceProtectionDetails_Dealer").attachPatternMatched(this._onObjectMatched, this);
