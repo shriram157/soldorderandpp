b71c16c0c79a228ac91e05da4ea71f4c9a0a5ca8
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2023-11-23 09:41:33 +0000

    changes for endpoint for CRQA

--- soldOrder/webapp/controller/NationalFleetSoldOrderView.controller.js
+++ soldOrder/webapp/controller/NationalFleetSoldOrderView.controller.js
@@ -101,7 +101,7 @@ sap.ui.define([
 					// 	//----------------------------------------------------------
 					// 	if (NFSO_controller.getView().getElementBinding('mainservices').getBoundContext().getProperty('Zzendcu')) {
 					// 		var zcustomerNumber = NFSO_controller.getView().getElementBinding('mainservices').getBoundContext().getProperty('Zzendcu');
-					// 		var url = "/node/tci/internal//node/api/v1.0/customer/cdms/customers/profile/" + zcustomerNumber;
+					// 		var url = "/node/api/v1.0/customer/cdms/customers/profile/" + zcustomerNumber;
 					// 		//?customerNumber=" +
 
 					// 		$.ajax({
